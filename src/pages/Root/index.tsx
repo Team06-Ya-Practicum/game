1ab9d5d330a09be3a1ededd2b948fc1757942b57
Alexander Berezin <alexanderberezin97@yandex.ru>
2022-04-04 08:44:47 +0300

    Style fix

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import cn from 'classnames';
-import { Container, Card, Row, Col, Image } from 'react-bootstrap';
+import {
+    Container, Card, Row, Col, Image,
+} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import characterImage from '../../img/character_front.svg';
 import backgroundImage from '../../img/road_ready.svg';
