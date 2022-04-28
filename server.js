21306e521e6d8629acfc8d5d9690dd2d2fe220c5
Alexander Berezin <alexanderberezin97@yandex.ru>
2022-04-28 11:30:53 +0300

    History api fix

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.set('port', process.env.PORT || 8080);
+app.all('*', (req, res) => {
+    res.sendFile('build/index.html', { root: __dirname });
+});
 
-const server = app.listen(app.get('port'), () => {
+const server = app.listen(process.env.PORT || 8080, () => {
     console.log('listening on port ', server.address().port);
 });
